122eda344b40ed5e3e47836015ee68e8ccd02084
Wing Kai <cyj9385@hotmail.com>
2018-12-29 19:08:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,65 @@ const downloader = (function() {
   }
 
   const rename = num => num > 99 ? num : num > 9 ? '0' + num : '00' + num
+  const simulateEvent = (() => {
+    function simulate(element, eventName) {
+      var options = extend(defaultOptions, arguments[2] || {});
+      var oEvent, eventType = null;
+
+      for (var name in eventMatchers) {
+        if (eventMatchers[name].test(eventName)) {
+          eventType = name;
+          break;
+        }
+      }
+
+      if (!eventType)
+        throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+      if (document.createEvent) {
+        oEvent = document.createEvent(eventType);
+        if (eventType == 'HTMLEvents') {
+          oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+        } else {
+          oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+            options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+            options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+        }
+        element.dispatchEvent(oEvent);
+      } else {
+        options.clientX = options.pointerX;
+        options.clientY = options.pointerY;
+        var evt = document.createEventObject();
+        oEvent = extend(evt, options);
+        element.fireEvent('on' + eventName, oEvent);
+      }
+      return element;
+    }
+
+    function extend(destination, source) {
+      for (var property in source)
+        destination[property] = source[property];
+      return destination;
+    }
+
+    var eventMatchers = {
+      'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+      'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+    }
+    var defaultOptions = {
+      pointerX: 0,
+      pointerY: 0,
+      button: 0,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      bubbles: true,
+      cancelable: true
+    }
+
+    return simulate
+  })()
 
   const showTips = {
     info (msg) {
@@ -106,6 +165,10 @@ const downloader = (function() {
         showTips.success('下载页面: ' + pageIndex);
         pageIndex += 1;
       }
+
+      simulateEvent(viewerCanvas, 'mousedown', { pointerX: 300, pointerY: 200 })
+      simulateEvent(viewerCanvas, 'mouseup', { pointerX: 300, pointerY: 200 })
+      handleClick()
     }, 200);
   }
 
